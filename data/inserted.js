5198d2bc92a9c30a5f0d4306a2f6fae47e65a766
Jonathan Metzman <jon.metzman@gmail.com>
2013-10-07 19:53:54 -0400

    Cleaned up functions in inserted and main

--- data/inserted.js
+++ data/inserted.js
@@ -1,14 +1,17 @@
-function checkAndReplace(ele) {
+var checkAndReplace = function (ele) {
     var autocomplete = ele.autocomplete;
     if (autocomplete !== "" && autocomplete != "on")
-        ele.autocomplete = "on";
-}
-
-var forms = document.forms;
-var i;
-for (i=0; i<=forms.length;i++) {
-    var form = forms[i];
-    checkAndReplace(form);
-    checkAndReplace(form.password);
-    checkAndReplace(form.username);
+	ele.autocomplete = "on";
 };
+
+var checkElements = function() {
+    var forms = document.forms;
+    for (var i = 0; i <= forms.length; i++) {
+	var form = forms[i];
+	checkAndReplace(form);
+	checkAndReplace(form.password);
+	checkAndReplace(form.username);
+    };
+};
+
+checkElements(); 
