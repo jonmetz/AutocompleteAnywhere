67bdc9fb530b13231c18b25f6240be4848e90fee
ThiefMaster <adrian@planetcoding.net>
2013-12-03 17:20:34 +0100

    Fix variable name typo
    Also fix the messy tabs+spaces mix

--- data/inserted.js
+++ data/inserted.js
@@ -1,14 +1,15 @@
 var checkAndReplace = function (element) {
     var autocomplete = element.autocomplete;
-    if (autocomplete !== "" && autocomplete != "on")
-	elemen.autocomplete = "on";
+    if (autocomplete !== "" && autocomplete != "on") {
+        element.autocomplete = "on";
+    }
 };
 
 var checkElements = function() {
     var elements = document.querySelectorAll('input[autocomplete="off"]');
     for (var i = 0; i < elements.length; i += 1) {
-	checkAndReplace(elements[i]);
-    };
+        checkAndReplace(elements[i]);
+    }
 };
 
 checkElements();
