1618530107ac11f3837e0d6319035c20c295f4d8
Jonathan Metzman <jon.metzman@gmail.com>
2013-12-03 17:11:41 -0500

    Made spacing consistent and added comments

--- data/turnOn.js
+++ data/turnOn.js
@@ -1,6 +1,6 @@
 var fixElements = function() {
-  var elements = Array.prototype.slice.call(document.querySelectorAll('input[autocomplete="off"]'));
-  elements = elements.concat(Array.prototype.slice.call(document.querySelectorAll('form[autocomplete="off"]')));
+  var elements = Array.prototype.slice.call(document.querySelectorAll('input[autocomplete="off"]')); // Get all the input fields with auto turned off in an array
+  elements = elements.concat(Array.prototype.slice.call(document.querySelectorAll('form[autocomplete="off"]'))); // concatinate the array with an array containing all the forms with auto turned off
   for (var i = 0; i < elements.length; i += 1) {
     elements[i].autocomplete="on";
   };
