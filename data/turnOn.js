d5af85add1cf3cd2d1fca61215265454662f5dfd
Jonathan Metzman <jon.metzman@gmail.com>
2013-12-03 16:21:46 -0500

    renamed variables to be less confusing

--- data/turnOn.js
+++ data/turnOn.js
@@ -1,17 +1,17 @@
-var checkAndReplace = function (elemen) {
-    var autocomplete = elemen.autocomplete;
-    if (autocomplete !== "" && autocomplete != "on")
-	elemen.autocomplete = "on";
+var checkAndReplace = function (element) {
+  var autocompleteStatus = element.autocomplete;
+  if (autocompleteStatus !== "" && autocompleteStatus != "on")
+    element.autocomplete = "on";
 };
 
 var checkElements = function() {
-    var forms = document.forms;
-    for (var i = 0; i <= forms.length; i++) {
-	var form = forms[i];
-	checkAndReplace(form);
-	checkAndReplace(form.password);
-	checkAndReplace(form.username);
-    };
+  var forms = document.forms;
+  for (var i = 0; i <= forms.length; i += 1) {
+    var form = forms[i];
+    checkAndReplace(form);
+    checkAndReplace(form.password);
+    checkAndReplace(form.username);
+  };
 };
 
-checkElements(); 
+checkElements();
